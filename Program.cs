db00b19982bbf1750d08dde7667fef846ec5e44d
Ahmad Raza <razaahmad1998@gmail.com>
2022-03-31 11:15:02 +0100

    added an OR to the if loops

--- Program.cs
+++ Program.cs
@@ -15,11 +15,11 @@ namespace Starting
             Console.WriteLine("1 litre of pain covers approx 12m^2");
             double paintNeeded = Math.Round(total/12);            
             Console.WriteLine("You will need "+ paintNeeded + " litres of paint");
-            Console.WriteLine("You will need + " + paintNeeded + " of the small size or " + (paintNeeded/5) + " of medium size or " + (paintNeeded/10) + " of large size can" );
+            Console.WriteLine("You will need "+ paintNeeded + " of the small size or " + (paintNeeded/5) + " of medium size or " + (paintNeeded/10) + " of large size can" );
             Console.WriteLine("What size paint would you like? small for 1 litre, medium for 5 litre or large for 10 litre");
             string size = Console.ReadLine();
             
-            if (size == "small" )
+            if (size == "small" || size == "1" )
             {
                 Console.WriteLine("Would you like the £8 single coat, the £10 double coat or the £20 Finish?");
                 int price = int.Parse(Console.ReadLine());
@@ -41,7 +41,7 @@ namespace Starting
                 }
                 Console.ReadLine();
             }
-            else if (size == "medium")
+            else if (size == "medium" || size == "5")
             {
                 Console.WriteLine("Would you like the £10 single coat, the £15 double coat or the £25 Finish?");
                 int price = int.Parse(Console.ReadLine());
@@ -63,7 +63,7 @@ namespace Starting
                 }
                 Console.ReadLine();
             }
-            else if (size == "large")
+            else if (size == "large" || size == "10")
             {
                 Console.WriteLine("Would you like the £15 single coat, the £20 double coat or the £30 Finish?");
                 int price = int.Parse(Console.ReadLine());
